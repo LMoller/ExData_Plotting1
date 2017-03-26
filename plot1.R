0f746d152bd8a70b580b0d6b9e7566ed352a3a31
LMoller <lakmoller@gmail.com>
2017-03-25 21:31:09 -0600

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,15 @@
+##Download file from link and unzip the file
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+powerfile <- unzip(temp)
+unlink(temp)
+
+##Read the file
+powerconsumption <- read.table(powerfile, header=T, sep=";")
+subsetpowerconsump <- powerconsumption[powerconsumption$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Plot 1
+globalActivePower <- as.numeric(subsetpowerconsump$Global_active_power) 
+png("plot1.png", width=480, height=480) 
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)") 
+dev.off() 
